84f06bf9c1cff6e8ab9bc6b992b0ef01b90e97d0
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2018-03-29 21:15:38 +0900

    [add] [big] [N] add config gui Why:config is needed Detail: electron and bootstrap,jquery

--- settings/app.js
+++ settings/app.js
@@ -1,1 +1,15 @@
+const electron = require('electron')
+// const app = electron.app
 console.log('eslint test')
+const BrowserWindow = electron.BrowserWindow
+
+electron.app.on('ready', () => {
+    var a = new BrowserWindow({ width:500, height:500, resizable: true,
+        // dirty hack for electron jquery not loaded
+        webPreferences: {
+            nodeIntegration: false
+        }
+    })
+    console.log(electron.shell)
+    a.loadURL('file://' + __dirname + '/index.html')
+})
